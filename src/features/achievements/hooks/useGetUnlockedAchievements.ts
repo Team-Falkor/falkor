64c4fd2dbecb08e0100047f129b6295950a3b23a
prostarz <tommyspam2000@gmail.com>
2024-12-30 05:17:49 +0000

    feat: disable queries if no id

--- src/features/achievements/hooks/useGetUnlockedAchievements.ts
+++ src/features/achievements/hooks/useGetUnlockedAchievements.ts
@@ -3,14 +3,20 @@ import { invoke } from "@/lib";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetUnlockedAchievements = (gameId: string) => {
+  const fetcher = async (): Promise<AchievementDBItem[]> => {
+    const unlocked = await invoke<AchievementDBItem[], string>(
+      "achievements:get-unlocked",
+      gameId
+    );
+    return unlocked ?? [];
+  };
+
   return useQuery({
     queryKey: ["achievements", "unlocked", gameId],
-    queryFn: async () => {
-      const unlocked = await invoke<AchievementDBItem[], string>(
-        "achievements:get-unlocked",
-        gameId
-      );
-      return unlocked ?? [];
-    },
+    queryFn: fetcher,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    enabled: !!gameId,
   });
 };
