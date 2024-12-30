cf408f2e56f099e00237be01e512c58145ac64db
prostarz <tommyspam2000@gmail.com>
2024-12-30 05:31:54 +0000

    fix: build errors

--- src/features/search/components/card.tsx
+++ src/features/search/components/card.tsx
@@ -15,11 +15,12 @@ const SearchCard = ({
     <Link
       className="w-full px-6 py-2 border-b rounded-md cursor-default select-none hover:cursor-pointer hover:text-white"
       key={1}
-      to={`/info/${id}`}
+      to={`/info/$id`}
+      params={{ id: id.toString() }}
       onClick={() => setOpen(false)}
     >
       <div className="flex gap-1.5">
-        <p className="text-sm flex-1 line-clamp-2">{name}</p>
+        <p className="flex-1 text-sm line-clamp-2">{name}</p>
         <span className="text-xs text-muted-foreground">({year})</span>
       </div>
     </Link>
