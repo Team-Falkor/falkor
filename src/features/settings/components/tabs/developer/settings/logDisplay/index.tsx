f296c7bdb36f406c46ca2c0dc6a8c457007d4132
prostarz <tommyspam2000@gmail.com>
2025-01-07 21:59:43 +0000

    feat(logs): clear logs

--- src/features/settings/components/tabs/developer/settings/logDisplay/index.tsx
+++ src/features/settings/components/tabs/developer/settings/logDisplay/index.tsx
@@ -1,10 +1,14 @@
+import Confirmation from "@/components/confirmation";
+import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { useLanguageContext } from "@/contexts/I18N";
+import { useLogger } from "@/hooks";
 import { useEffect, useState } from "react";
 import LogWindow from "./logWindow";
 
 const LogDisplay = () => {
+  const { removeLogs } = useLogger();
   const { t } = useLanguageContext();
 
   const getInitialEnabledState = () => {
@@ -25,16 +29,23 @@ const LogDisplay = () => {
 
   return (
     <div className="flex flex-col" id="developer-settings">
-      <div className="flex items-center gap-2" id="enable-dev-console">
-        <Switch
-          id="enable-dev-console"
-          onCheckedChange={onCheckedChange}
-          checked={enabled}
-        />
-        <Label htmlFor="enable-dev-console">
-          {t("settings.settings.enable_developer_console")}
-        </Label>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-2" id="enable-dev-console">
+          <Switch
+            id="enable-dev-console"
+            onCheckedChange={onCheckedChange}
+            checked={enabled}
+          />
+          <Label htmlFor="enable-dev-console">
+            {t("settings.settings.enable_developer_console")}
+          </Label>
+        </div>
+
+        <Confirmation onConfirm={removeLogs}>
+          <Button>Clear Logs</Button>
+        </Confirmation>
       </div>
+
       <LogWindow enabled={enabled} />
     </div>
   );
