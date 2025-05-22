884d829c5952d669719c36967b61f049a510f7b5
prostarz <tommyspam2000@gmail.com>
2025-05-22 06:20:05 +0100

    refactor: remove unused useEffect hook from Index component
    The useEffect hook was previously used to throw a test error, which is no longer needed. This cleanup improves code maintainability by removing unnecessary code.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect } from "react";
 import MainContainer from "@/components/containers/mainContainer";
 import FeaturedGames from "@/features/home/components/FeaturedGames";
 import GameCategories from "@/features/home/components/GameCategories";
@@ -9,10 +8,6 @@ import HeroSection from "@/features/home/components/HeroSection";
 export const Route = createFileRoute("/")({ component: Index });
 
 function Index() {
-	useEffect(() => {
-		throw new Error("Test error");
-	}, []);
-
 	return (
 		<MainContainer id="main-page" className="w-full overflow-hidden">
 			<HeroSection />
