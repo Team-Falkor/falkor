ddd6cdf77c26f9050f0c8f6573a9d59e57cbd42d
prostarz <tommyspam2000@gmail.com>
2025-06-01 12:17:48 +0100

    feat(achievements): add achievement importer and update types
    - Add new AchievementImporter class to handle importing achievements for user's games
    - Update type definitions to better reflect Steam API response structures
    - Add getUserAchievements method to AchievementData class to fetch player-specific achievements
    - Implement logic to filter and process unlocked achievements for each game

--- electron/handlers/achievements/data.ts
+++ electron/handlers/achievements/data.ts
@@ -1,4 +1,7 @@
-import type { ISchemaForGame } from "@/@types";
+import type {
+	IGetPlayerAchievementsResponse,
+	IGetSchemaForGame,
+} from "@/@types";
 import logger from "../logging";
 import { SettingsManager } from "../settings/settings";
 
@@ -7,7 +10,7 @@ const settings = SettingsManager.getInstance();
 class AchievementData {
 	readonly api_url: string | undefined = settings.get("api_base_url");
 
-	async get(steamId: string, lang = "en"): Promise<ISchemaForGame> {
+	async get(steamId: string, lang = "en"): Promise<IGetSchemaForGame> {
 		try {
 			const url = `${this.api_url}/achievements/${steamId}?lang=${lang}`;
 
@@ -17,12 +20,38 @@ class AchievementData {
 
 			if (!request.ok) throw new Error(request.statusText);
 
-			const data: ISchemaForGame = await request.json();
+			const data: IGetSchemaForGame = await request.json();
 
 			return data;
 		} catch (error) {
 			console.log(error);
 			logger.log("error", `Error getting achievement data: ${error}`);
+			throw error;
+		}
+	}
+
+	async getUserAchievements(
+		steamId: string,
+		appId: number,
+		lang = "en",
+	): Promise<IGetPlayerAchievementsResponse | null> {
+		try {
+			const url = `${this.api_url}/achievements/user/${steamId}/game/${appId}?lang=${lang}`;
+
+			const request = await fetch(url, {
+				method: "GET",
+			});
+
+			if (!request.ok) {
+				console.log("no achievements found");
+				return null;
+			}
+
+			const data: IGetPlayerAchievementsResponse = await request.json();
+			return data;
+		} catch (error) {
+			console.log(error);
+			logger.log("error", `Error getting user achievements: ${error}`);
 			throw error;
 		}
 	}
