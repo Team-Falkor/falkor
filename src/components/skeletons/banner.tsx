0fb3794a5eb98bd35423679a209078f9e54864bf
prostarz <tommyspam2000@gmail.com>
2025-06-14 19:12:13 +0100

    style(skeletons): adjust banner skeleton height to 28rem
    The height was changed from h-80 to h-[28rem] to better match the design specifications

--- src/components/skeletons/banner.tsx
+++ src/components/skeletons/banner.tsx
@@ -1,7 +1,7 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 const BannerSkeleton = () => {
-	return <Skeleton className="h-80 w-full rounded-lg" />;
+	return <Skeleton className="h-[28rem] w-full rounded-lg" />;
 };
 
 export default BannerSkeleton;
