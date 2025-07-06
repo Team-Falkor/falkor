21e53a374697fe51c26d3003e22d9324d1adcd92
prostarz <prostarz@proton.me>
2025-07-06 21:48:24 +0100

    fix: change "remove" to "delete" in delete dialog for clarity

--- src/components/cards/list-card/overlay/delete.tsx
+++ src/components/cards/list-card/overlay/delete.tsx
@@ -23,7 +23,7 @@ const DeleteDialog = ({ gameId }: DeleteDialogProps) => {
 	return (
 		<Confirmation onConfirm={() => deleteGame({ gameId })}>
 			<DropdownMenuItem onSelect={(e) => e.preventDefault()}>
-				{t("remove")}
+				{t("delete")}
 			</DropdownMenuItem>
 		</Confirmation>
 	);
